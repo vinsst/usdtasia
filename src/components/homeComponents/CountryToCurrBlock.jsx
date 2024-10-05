5cf7cf405daa1227d56e0dbd07fe1b42fb2d2aeb
vinsst <vinsstcompany@gmail.com>
2024-10-05 17:44:50 +0300

    currentExchRatesFullyDone

--- src/components/homeComponents/CountryToCurrBlock.jsx
+++ src/components/homeComponents/CountryToCurrBlock.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import see_more from "../../assets/img/see-more-ic.svg";
 import graph from "../../assets/img/graph.svg";
 
+import { useSelector } from "react-redux";
 function CountryToCurrBlock({ symbol, curr }) {
+  const selectedCurrency = useSelector(
+    (state) => state.currCryptoRatesReducer.crypto
+  );
+
+  const priceArray = useSelector((state) => state.exchangeReducer.rates);
+
+  const gettingPrice = () => {
+    const rateObj = priceArray.find(
+      (obj) => obj.from === selectedCurrency && obj.to === symbol
+    );
+    return rateObj ? rateObj.rate.toFixed(2) : "N/A";
+  };
+
   return (
     <div className="countryToUsdt_block">
       <div className="countryToUsdt__block_side1">
@@ -13,11 +27,6 @@ function CountryToCurrBlock({ symbol, curr }) {
             className="countryToUsdt__block_side1_left_coin"
           />
           <p className="countryToUsdt__block_side1_left_abr">{symbol}</p>
-          {/* <img
-            src={flag}
-            alt=""
-            className="countryToUsdt__block_side1_left_ctr"
-          /> */}
         </div>
         <div className="countryToUsdt__block_side1_right">
           <img src={see_more} alt="" className="" />
@@ -26,8 +35,12 @@ function CountryToCurrBlock({ symbol, curr }) {
       <div className="countryToUsdt__block_line"></div>
       <div className="countryToUsdt__block_side2">
         <div className="countryToUsdt__block_side2_txt">
-          <p className="countryToUsdt__block_side2_txt1">1 USDT =</p>
-          <div className="countryToUsdt__block_side2_txt2">1,80 {symbol}</div>
+          <p className="countryToUsdt__block_side2_txt1">
+            1 {selectedCurrency} =
+          </p>
+          <div className="countryToUsdt__block_side2_txt2">
+            {gettingPrice()} {symbol}
+          </div>
         </div>
         <img src={graph} alt="" className="countryToUsdt__block_side2_graph" />
       </div>
