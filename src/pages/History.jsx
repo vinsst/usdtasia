13775fc240f925f17df052dbc37a47373d638660
vinsst <vinsstcompany@gmail.com>
2024-10-10 16:35:46 +0300

    history

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,29 +1,38 @@
 import React, { useEffect, useState } from "react";
 import HistoryLine from "../components/history/HistoryLine";
 
+import arrRight from "../assets/img/arrRight.svg";
+
+import { useSelector } from "react-redux";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
 function History() {
   const [transactions, setTransactions] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const loginTxt = useSelector((state) => state.loginReducer.login);
 
   const token =
     localStorage.getItem("authToken") || sessionStorage.getItem("authToken");
+
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchData = async (page) => {
       if (token) {
         const decodedToken = jwtDecode(token);
         const id = decodedToken.id;
 
         try {
           const response = await axios.get(
-            `https://usdtasia-back-8a0cb4592177.herokuapp.com/transaction/${id}?Page=1`,
+            `https://usdtasia-back-8a0cb4592177.herokuapp.com/transaction/${id}?Page=${page}`,
             {
               headers: { Authorization: `Bearer ${token}` },
             }
           );
           setTransactions(response.data.transactions);
+          setTotalPages(response.data.pages);
         } catch (error) {
           console.error("Error fetching data:", error);
         } finally {
@@ -32,10 +41,27 @@ function History() {
       }
     };
 
-    fetchData();
-  }, [token]);
+    fetchData(currentPage);
+  }, [token, loginTxt, currentPage]);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <main className="homeMain home_container container other_container">
+        <p className="loading_history">Loading...</p>
+      </main>
+    );
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
     <main className="homeMain home_container container other_container">
@@ -66,6 +92,40 @@ function History() {
                 />
               );
             })}
+            {transactions.length === 0 && (
+              <div className="table_zeroTransactions">
+                <p className="table_zeroTransactions_p">
+                  Вы еще не создавали транзакции
+                </p>
+              </div>
+            )}
+            <div className="table_pageScroller">
+              <img
+                src={arrRight}
+                alt=""
+                className="pageScroller_left pageScroller_arr"
+                onClick={handlePrevPage}
+                style={{ opacity: currentPage === 1 ? 0.5 : 1 }}
+              />
+              {Array.from({ length: totalPages }, (_, i) => (
+                <div
+                  key={i}
+                  className={`pageScroller_number ${
+                    currentPage === i + 1 ? "pageScroller_number_active" : ""
+                  }`}
+                  onClick={() => setCurrentPage(i + 1)}
+                >
+                  {i + 1}
+                </div>
+              ))}
+              <img
+                src={arrRight}
+                alt=""
+                className="pageScroller_right pageScroller_arr"
+                onClick={handleNextPage}
+                style={{ opacity: currentPage === totalPages ? 0.5 : 1 }}
+              />
+            </div>
           </section>
         </div>
       </div>
