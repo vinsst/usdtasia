d6b9f0c0a25722883d69bf68025ec6ee4e9d971c
vinsst <vinsstcompany@gmail.com>
2024-11-29 14:47:32 +0300

    history_adapt

--- src/components/history/HistoryLine.jsx
+++ src/components/history/HistoryLine.jsx
@@ -22,9 +22,13 @@ function HistoryLine({ createdAt, from, to, status, darker, transactionId }) {
       <p className="table__content_line_el history__blue_font table__content_line_el_from">
         {from.name}
       </p>
-      <p className="table__content_line_el">{from.value.toFixed(2)}</p>
+      <p className="table__content_line_el table__content_line_el_value">
+        {from.value.toFixed(2)}
+      </p>
       <p className="table__content_line_el history__blue_font">{to.name}</p>
-      <p className="table__content_line_el">{to.value.toFixed(2)}</p>
+      <p className="table__content_line_el table__content_line_el_value">
+        {to.value.toFixed(2)}
+      </p>
       <p
         className={`table__content_line_el  history__green_font ${statusMap[status]}`}
       >
